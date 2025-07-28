b818ea0e3628ab1fad75c57367f133ff9caeca72
Abdelrahman-hass <abdelrahmanhassanein2@gmail.com>
2025-07-28 21:57:05 +0300

    Integrated PostgreSQL and created admin account

--- app.py
+++ app.py
@@ -8,6 +8,13 @@ from werkzeug.security import generate_password_hash
 
 app = create_app()
 
+
+# Set maximum video upload size to 1 GB
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 * 1024  # 1 GB
+
+# Register Flask-Migrate
+migrate = Migrate(app, db)
+
 @app.before_first_request
 def create_admin():
     with app.app_context():
@@ -23,12 +30,7 @@ def create_admin():
             print("⚡ Admin created:", admin.username)
         else:
             print("⚠️ Admin already exists.")
-
-# Set maximum video upload size to 1 GB
-app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 * 1024  # 1 GB
-
-# Register Flask-Migrate
-migrate = Migrate(app, db)
+            
 
 if __name__ == "__main__":
     app.run(debug=True)
