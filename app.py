2f9b170cee4d5121fbf6b7ae1ddb90caf038c6e4
Abdelrahman-hass <abdelrahmanhassanein2@gmail.com>
2025-07-28 21:35:35 +0300

    Debug admin creation

--- app.py
+++ app.py
@@ -21,12 +21,15 @@ if __name__ == "__main__":
 @app.before_first_request
 def create_admin():
     with app.app_context():
-        if not User.query.filter_by(username="admin").first():
+        if not User.query.filter_by(username="ARNcode").first():
             admin = User(
                 username="ARNcode",
                 email="arncode.app@gmail.com",
-                password=generate_password_hash("Abd123an0", method="sha256"),
+                password=generate_password_hash("Abd123an@", method="sha256"),
                 is_admin=True
             )
             db.session.add(admin)
             db.session.commit()
+            print("⚡ Admin created:", admin.username)
+        else:
+            print("⚠️ Admin already exists.")
